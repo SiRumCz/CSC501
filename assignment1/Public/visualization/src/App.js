8658e8f41bdf7bd0af482c0afb0e87751fa2a085
soroush <soroushysfi@gmail.com>
2019-09-18 15:17:19 -0700

    simple bar chart with sample data

--- assignment1/Public/visualization/src/App.js
+++ assignment1/Public/visualization/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-import { BarChart } from './components/BarChart-component';
+import { BarChart } from './components/BarChart.component';
 
-function App() {
-  return (
-    <div className="App">
-       <h1> This is going to be an awesome visualization</h1>
-        <p>And this is an example:</p>
-        <BarChart></BarChart>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            barData: [
+                {title: 1, value: 243}, {title: 2, value: 187}, {title: 3, value: 234}, {title: 4, value: 104}, {title: 5, value: 83}
+                ]
+            ,
+            width: 700,
+            height: 300
+        }
+    }
+
+    render () {
+        return(
+        <div className="App">
+            <h1> Bar chart for movies by ranking:</h1>
+            <BarChart width={700} height={300} data={this.state.barData}/>
+        </div>
+        )
+    }
 }
 
 export default App;
