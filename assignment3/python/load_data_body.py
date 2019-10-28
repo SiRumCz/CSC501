61da5dee3317d32055489f8b88d9b358df961a6f
noonespecial009 <noonespecial009@gmail.com>
2019-10-27 17:44:05 -0700

    need to fix python talking to neo4j

--- assignment3/python/load_data_body.py
+++ assignment3/python/load_data_body.py
@@ -1,9 +1,15 @@
 from py2neo import Graph, Node, Relationship
-import pandas as pd 
-
+# import pandas as pd 
+import time
+print("Printed immediately.")
+time.sleep(45)
+print("Printed after 60 seconds.")
 ''' insert your own password here - you can set it at localhost:7474 '''
-graph = Graph(password="password")
-
+#graph = Graph(password="password")
+#graph = Graph('neo4j_url')
+#graph = Graph("bolt://172.17.0.3:7687")
+graph = Graph(host="http://localhost:7474", auth=("neo4j", "password")) 
+#graph = Graph("bolt://127.0.0.1:7687", auth=("neo4j", "password"))
 ''' read the .tsv into panda '''
 # data_rider = pd.read_csv('https://snap.stanford.edu/data/soc-redditHyperlinks-body.tsv')
 # print(data_rider.head(3))
